b0d6547d3657253f03dca75e17f223ba3d67333e
huayu <369671231@qq.com>
2021-10-22 09:49:58 -0400

    Lab6: Multithreading -- Using threads

--- notxv6/ph.c
+++ notxv6/ph.c
@@ -13,6 +13,9 @@ struct entry {
   int value;
   struct entry *next;
 };
+
+pthread_mutex_t lock[NBUCKET];
+
 struct entry *table[NBUCKET];
 int keys[NKEYS];
 int nthread = 1;
@@ -43,6 +46,7 @@ void put(int key, int value)
 
   // is the key already present?
   struct entry *e = 0;
+  pthread_mutex_lock(&lock[i]);
   for (e = table[i]; e != 0; e = e->next) {
     if (e->key == key)
       break;
@@ -55,6 +59,7 @@ void put(int key, int value)
     insert(key, value, &table[i], table[i]);
   }
 
+  pthread_mutex_unlock(&lock[i]);
 }
 
 static struct entry*
@@ -116,6 +121,10 @@ main(int argc, char *argv[])
   assert(NKEYS % nthread == 0);
   for (int i = 0; i < NKEYS; i++) {
     keys[i] = random();
+  }
+  
+  for(int i = 0; i < NBUCKET; i++){
+    pthread_mutex_init(&lock[i], NULL);
   }
 
   //
