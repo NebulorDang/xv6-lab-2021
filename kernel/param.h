46bcbaf1f2b638ede67e3dd1fcccee226a772fa5
Jonathan Behrens <fintelia@gmail.com>
2021-11-17 22:03:12 -0500

    Release lab-fs

--- kernel/param.h
+++ kernel/param.h
@@ -1,4 +1,8 @@
-#define NPROC        64  // maximum number of processes
+#ifdef LAB_FS
+#define NPROC        10  // maximum number of processes
+#else
+#define NPROC        64  // maximum number of processes (speedsup bigfile)
+#endif
 #define NCPU          8  // maximum number of CPUs
 #define NOFILE       16  // open files per process
 #define NFILE       100  // open files per system
@@ -9,5 +13,15 @@
 #define MAXOPBLOCKS  10  // max # of blocks any FS op writes
 #define LOGSIZE      (MAXOPBLOCKS*3)  // max data blocks in on-disk log
 #define NBUF         (MAXOPBLOCKS*3)  // size of disk block cache
-#define FSSIZE       1000  // size of file system in blocks
+#ifdef LAB_FS
+#define FSSIZE       200000  // size of file system in blocks
+#else
+#ifdef LAB_LOCK
+#define FSSIZE       10000  // size of file system in blocks
+#else
+#define FSSIZE       2000   // size of file system in blocks
+#endif
+#endif
 #define MAXPATH      128   // maximum file path name
+
+
